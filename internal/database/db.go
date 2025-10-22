46df1161d905a752dbffe373305a3c75c3484750
stasgramma <amberbober0@gmail.com>
2025-10-22 20:01:17 +0300

    1.0.1

--- internal/database/db.go
+++ internal/database/db.go
@@ -16,11 +16,25 @@ func Connect(cfg *config.Config) {
 	var err error
 	DB, err = gorm.Open(postgres.Open(cfg.GetDSN()), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("Error: Gorm.Open: %v", err)
+		log.Fatalf("❌ Error: Gorm.Open: %v", err)
 	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Error: DB.DB(): %v", err)
+	}
+
+	// Проверяем, доступна ли база реально
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatalf("Database unreachable: %v", err)
+	}
+
+	log.Println("Connected to PostgreSQL successfully")
 
 	// Авто-миграция таблиц
 	if err := DB.AutoMigrate(&models.User{}); err != nil {
 		log.Fatalf("Error on migration: %v", err)
 	}
+
+	log.Println("✅ Auto-migration completed")
 }
